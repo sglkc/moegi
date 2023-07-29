ff98bc145db6414313cdc394abd68a1f93800158
Seya <31957516+sglkc@users.noreply.github.com>
2023-07-29 09:50:56 +0700

    feat: migrate src/popup
    chore: remove unused lib directory and manifest file

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import '@unocss/reset/tailwind-compat.css'
-import './index.css'
-import 'virtual:uno.css'
-
-render(<App />, document.getElementById('app')!)
