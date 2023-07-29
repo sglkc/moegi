a40fb42288aba1c48f02d1f11f5832c4357ca9de
Seya <31957516+sglkc@users.noreply.github.com>
2023-07-29 10:18:30 +0700

    feat: change manifest to match package.json

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,39 +1,42 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 import packageJson from './package.json'
 
-const { version } = packageJson
-
-// Convert from Semver (example: 0.1.0-beta6)
+const { name, description, homepage, version } = packageJson
+const nameCap = name[0].toUpperCase() + name.slice(1);
 const [major, minor, patch, label = '0'] = version
   .replace(/[^\d.-]+/g, '')
   .split(/[.-]/)
 
-export default defineManifest((env) => ({
-  manifest_version: 3,
-  name: (env.mode !== 'production') ? '[DEV] Moegi' : 'Moegi',
-  version: `${major}.${minor}.${patch}.${label}`,
-  version_name: version,
-  description: packageJson.description,
-  homepage_url: packageJson.homepage,
-  icons: {
-    16: 'icons/icon16.png',
-    19: 'icons/icon19.png',
-    48: 'icons/icon48.png',
-    128: 'icons/icon128.png'
-  },
-  default_locale: 'en',
-  action: {
-    default_icon: {
+export default defineManifest((env) => {
+  const nameExt = (env.mode !== 'production' ? '[DEV] ' : '') + nameCap;
+
+  return {
+    manifest_version: 3,
+    name: nameExt,
+    version: `${major}.${minor}.${patch}.${label}`,
+    version_name: version,
+    description: description,
+    homepage_url: homepage,
+    icons: {
       16: 'icons/icon16.png',
       19: 'icons/icon19.png',
       48: 'icons/icon48.png',
       128: 'icons/icon128.png'
     },
-    default_title: 'Moegi Settings',
-    default_popup: 'index.html'
-  },
-  permissions: [
-    'storage',
-    'tabs'
-  ],
-}))
+    default_locale: 'en',
+    action: {
+      default_icon: {
+        16: 'icons/icon16.png',
+        19: 'icons/icon19.png',
+        48: 'icons/icon48.png',
+        128: 'icons/icon128.png'
+      },
+      default_title: nameExt,
+      default_popup: 'index.html'
+    },
+    permissions: [
+      'storage',
+      'tabs'
+    ],
+  }
+});
