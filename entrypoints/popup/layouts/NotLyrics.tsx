76ec2085b64e382f4f0709ff5b2c9a7af5e3890a
Seya <31957516+sglkc@users.noreply.github.com>
2025-02-09 18:36:14 +0700

    feat(popup): migrate popup components
    wip: form component

---
+++ entrypoints/popup/layouts/NotLyrics.tsx
@@ -0,0 +1,13 @@
+export default function NotLyrics() {
+  return (
+    <div class="flex flex-col gap-2">
+      <h1>
+        <strong>
+          Open the lyrics page (microphone icon at the bottom-right) to get
+          started!
+        </strong>
+      </h1>
+      <p>If you don't see it, then the song you choose doesn't have lyrics.</p>
+    </div>
+  )
+}
---
+++ entrypoints/popup/layouts/NotSpotify.tsx
@@ -0,0 +1,19 @@
+export default function NotSpotify() {
+  const openSpotify = () => chrome.tabs.create({
+    active: true,
+    url: 'https://open.spotify.com',
+  });
+
+  return (
+    <div class="flex flex-col gap-2">
+      <h1>
+        <strong>
+          Looks like you don't have Spotify opened in the current tab :/
+        </strong>
+      </h1>
+      <a class="underline text-accent cursor-pointer" onClick={openSpotify}>
+        Open Spotify Web Player
+      </a>
+    </div>
+  );
+}
