f9081208bfd64f09b13052785ee4c92df8ad9729
Seya <31957516+sglkc@users.noreply.github.com>
2025-06-28 12:47:44 +0700

    feat(change-option): allow multiple option changes
    fixed #24

--- entrypoints/background/change-option.ts
+++ entrypoints/background/change-option.ts
@@ -4,13 +4,17 @@ import { getDeepKeys } from '@/utils/deep-keys'
 import { Background, OptionsMessage } from '@/utils/messaging'
 import { MoegiOptions } from '@/utils/options'
 
-// TODO: move debouncing to storage.ts ??
-const sendOptionsToContent = debounce((tabId: number, data: OptionsMessage) => {
-  console.log('Forward options to content', data.changes, data.options)
-  Background.sendMessage('applyOptions', data, { tabId })
-}, 500)
-
+// store multiple changes
+let changes: OptionsMessage['changes'] = []
 let oldOptions: MoegiOptions | undefined
+
+const sendOptionsToContent = debounce((tabId: number, options: MoegiOptions) => {
+  if (!changes.length) return
+
+  Background.sendMessage('applyOptions', { changes, options }, { tabId })
+  console.log('Forward options to content', changes, options)
+  changes = []
+}, 500)
 
 Background.onMessage('applyOptions', async ({ data: { options } }) => {
   const [tab] = await chrome.tabs.query({
@@ -28,11 +32,15 @@ Background.onMessage('applyOptions', async ({ data: { options } }) => {
 
   // get updated keys then update old data
   const updates = updatedDiff(oldOptions, options)
-  const changes = getDeepKeys(updates)
+  const newChanges = getDeepKeys(updates)
+
+  // merge with old changes
+  for (const change of newChanges) {
+    if (changes.includes(change)) continue
+    changes.push(change)
+  }
+
   oldOptions = options
 
-  // Only send if there's any updates
-  if (!changes.length) return
-
-  sendOptionsToContent(tab.id, { changes, options })
+  sendOptionsToContent(tab.id, options)
 })
